ae9a750a5313768bea7d78ae64c03fac2580fbab
YaYaHuang <570911275@qq.com>
2019-10-15 13:20:59 +0800

    添加测试页面跳转与Json数据传输测试

--- src/main/java/com/redis/jedis_test_pom/controller/FirstController.java
+++ src/main/java/com/redis/jedis_test_pom/controller/FirstController.java
@@ -1,15 +1,27 @@
 package com.redis.jedis_test_pom.controller;
 
+import com.redis.jedis_test_pom.utils.StateParameter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(value = "/first")
-public class FirstController {
+public class FirstController extends BaseController{
     @RequestMapping("view")
     public String view(HttpServletRequest request) {
+        logger.info("进入测试页面");
         return "demoPage/firstPage";
+    }
+
+    @RequestMapping(value = "/back",method = RequestMethod.POST)
+    @ResponseBody
+    public ModelMap back(HttpServletRequest request){
+        logger.info("进入json测试页面");
+        return getModelMap(StateParameter.SUCCESS,null,"请求成功");
     }
 }
